b21b321785f0b79580680da7968941d59a9016db
Nguyen Hoang Vu <nhvu1988@gmail.com>
2017-06-03 15:36:35 +0700

    Add project files.

---
+++ Ext.Net.MVC.Sample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ext.Net.MVC.Sample.Startup))]
+namespace Ext.Net.MVC.Sample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
